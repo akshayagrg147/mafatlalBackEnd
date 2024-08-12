0c0d0430a70950f1052205ca055a3a9c032b6dbe
chandan sharma <chandan.sharma@orahi.com>
2024-08-12 11:02:12 +0530

    Implement all cors headers

--- mafatlal/mafatlal/settings.py
+++ mafatlal/mafatlal/settings.py
@@ -57,9 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mafatlal.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
