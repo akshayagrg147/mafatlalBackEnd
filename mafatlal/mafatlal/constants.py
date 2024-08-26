286de4b8310ec3f4a588c9fb0c1a9ded629ab79e
chandan sharma <chandan.sharma@orahi.com>
2024-08-26 12:26:12 +0530

    add constants

--- mafatlal/mafatlal/constants.py
+++ mafatlal/mafatlal/constants.py
@@ -7,6 +7,8 @@ INITAITED_PRODUCT_INFO_API      = "Initaited the product information screen api"
 INITAITED_ORDER_HISTORY_API     = "Initaited the order history api"
 INITAITED_PLACE_ORDER_API       = "Initaited the order place api"
 INITAITED_ORDER_DETAILS_API     = "Initaited the order details api"
+INITAITED_PLACE_LIST_API        = "Initaited the order details api"
+INITAITED_ORDER_STATS_API       = "Initaited the order details api"
 
 INITAITED_ADDRESS_INSERT_API    = "Initaited Address insert api"
 INITAITED_ADDRESS_UPDATE_API    = "Initaited Address update api"
